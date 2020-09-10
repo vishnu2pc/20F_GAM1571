8971f76e28d39edd154fc018adb982b7746aca95
Vishnu Jayaraj <vishnu2pc@gmail.com>
2020-09-10 12:25:07 -0500

    Finalized build project

--- CPPFramework/premake5.lua
+++ CPPFramework/premake5.lua
@@ -1,6 +1,16 @@
 workspace "Game"
 	configurations { "Debug", "Release" }
 	platforms {"x64"}
+	location "build"
+	characterset "MBCS"
 	
 project "Game"
 	kind "WindowedApp"
+	
+	files {
+		"Game/Source/**.cpp",
+		"GenerateProjectFiles.bat",
+		"premake5.lua",
+		".gitignore"
+		}
+		

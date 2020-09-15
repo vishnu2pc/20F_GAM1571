c187a5c927852970249f0736bd672508c7d2d008
Vishnu Jayaraj <vishnu2pc@gmail.com>
2020-09-14 22:31:47 -0400

    Setup our Initial Project.

---
+++ GenerateProjectFiles.bat
@@ -0,0 +1,3 @@
+premake5 vs2019
+
+pause
---
+++ premake5.lua
@@ -0,0 +1,14 @@
+workspace "Game"
+	configurations { "Debug","Release"}
+	platforms	{"x64"}
+	location	"build"
+	
+project "Game"
+	kind	"WindowedApp"
+
+	files {
+		"Game/Source/**.cpp",
+		"premake5.lua",
+		".gitignore",
+		"GenerateProjectFiles.bat",
+	}

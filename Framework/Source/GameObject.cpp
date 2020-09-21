4cca4832c216e5afca2266afaf2ecef3eed15550
Vishnu Jayaraj <vishnu2pc@gmail.com>
2020-09-21 00:49:47 -0400

    Completed Megaman Mesh

--- Framework/Source/GameObject.cpp
+++ Framework/Source/GameObject.cpp
@@ -6,15 +6,18 @@
 
 fw::GameObject::GameObject(int GameObjectType)
 {
-    m_pShader_Blue = new fw::ShaderProgram("Data/Basic.vert", "Data/Blue.frag");
+    
     m_pShader_DarkBlue = new fw::ShaderProgram("Data/Basic.vert", "Data/DarkBlue.frag");
+	m_pShader_Blue = new fw::ShaderProgram("Data/Basic.vert", "Data/Blue.frag");
 	m_pShader_Skin = new fw::ShaderProgram("Data/Basic.vert", "Data/Skin.frag");
+	m_pShader_Face = new fw::ShaderProgram("Data/Basic.vert", "Data/Black.frag");
 
 	if (GameObjectType == HUMANOID)
 	{
 		m_pMeshDarkBlue = new fw::Mesh(VERTEX_DARK_BLUE, 54, GL_TRIANGLES);
-		m_pMeshBlue = new fw::Mesh(VERTEX_BLUE, 36, GL_TRIANGLES);
-		
+		m_pMeshBlue = new fw::Mesh(VERTEX_BLUE, 42, GL_TRIANGLES);
+		m_pMeshSkin = new fw::Mesh(VERTEX_SKIN, 18, GL_TRIANGLES);
+		m_pMeshFace = new fw::Mesh(VERTEX_FACE, 24, GL_TRIANGLES);
 	}
 
 	if (GameObjectType == ANIMAL)
@@ -36,6 +39,7 @@ void fw::GameObject::Update()
 void fw::GameObject::Draw()
 {
 	m_pMeshDarkBlue->Draw(m_pShader_DarkBlue);
+	m_pMeshSkin->Draw(m_pShader_Skin);
+	m_pMeshFace->Draw(m_pShader_Face);
 	m_pMeshBlue->Draw(m_pShader_Blue);
-	
 }
--- Framework/Source/GameObject.h
+++ Framework/Source/GameObject.h
@@ -15,11 +15,13 @@ private:
     fw::ShaderProgram* m_pShader_Blue = nullptr;
     fw::ShaderProgram* m_pShader_DarkBlue = nullptr;
     fw::ShaderProgram* m_pShader_Skin = nullptr;
+    fw::ShaderProgram* m_pShader_Face = nullptr;
+		
 		
     fw::Mesh* m_pMeshDarkBlue = nullptr;
     fw::Mesh* m_pMeshBlue = nullptr;
     fw::Mesh* m_pMeshSkin = nullptr;
-		
+    fw::Mesh* m_pMeshFace = nullptr;
 
     float VERTEX_DARK_BLUE[108] =
     {
@@ -101,7 +103,7 @@ private:
     	
     };
 
-    float VERTEX_BLUE[72] =
+    float VERTEX_BLUE[84] =
     {
         -0.8f, -0.4f,
     	-0.7f, -0.3f,
@@ -152,9 +154,77 @@ private:
         -0.8f, 0.2f,
         -0.7f, 0.2f,
         -0.7f, 0.3f,
-    	
+
+    	-0.55f, 0.3f,
+    	-0.45f, 0.3f,
+    	-0.45f, 0.5f,
+
+        -0.55f, 0.3f,
+        -0.45f, 0.5f,
+    	-0.55f, 0.5f
+    };
+		
+    float VERTEX_SKIN[36]
+    {
+        -0.5f, -0.1f,
+        -0.7f, 0.1f,
+    	-0.7f, 0.2f,
+
+        -0.5f, -0.1f,
+        -0.3f, 0.1f,
+        -0.3f, 0.2f,
+
+        -0.7f, 0.2f,
+        -0.7f, 0.3f,
+        -0.5f, 0.3f,
+
+        -0.3f, 0.2f,
+        -0.3f, 0.3f,
+        -0.5f, 0.3f,
+
+        -0.7f, 0.2f,
+        -0.5f, 0.3f,
+        -0.5f, -0.1f,
+
+        -0.3f, 0.2f,
+        -0.5f, 0.3f,
+        -0.5f, -0.1f,
     };
 
+    float VERTEX_FACE[72]
+    {
+        -0.6f, 0.2f,
+        -0.6f, 0.15f,
+        -0.55f, 0.2f,
+
+         -0.4f, 0.2f,
+        -0.4f, 0.15f,
+        -0.45f, 0.2f,
+    	
+        -0.6f, 0.15f,
+        -0.55f, 0.2f,
+        -0.55f, 0.15f,
+
+        -0.4f, 0.15f,
+        -0.45f, 0.2f,
+        -0.45f, 0.15f,
+
+    	-0.55f, 0.0f,
+    	-0.5f, 0.0f,
+    	-0.55f, 0.05f,
+
+        -0.45f, 0.0f,
+        -0.5f, 0.0f,
+        -0.45f, 0.05f,
+
+        -0.5f, 0.0f,
+        -0.55f, 0.05f,
+    	-0.5f, 0.05f,
+
+        -0.5f, 0.0f,
+        -0.45f, 0.05f,
+        -0.5f, 0.05f
+    };
    
 	
 };

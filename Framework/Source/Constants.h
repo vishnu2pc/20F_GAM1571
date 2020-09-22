87643c6561405856365f2c94555a456b97b63929
Vishnu Jayaraj <vishnu2pc@gmail.com>
2020-09-21 22:22:08 -0400

    Added Constants.h

--- Framework/Source/Constants.h
+++ Framework/Source/Constants.h
@@ -1,4 +1,42 @@
 #pragma once
-enum GAME_OBJECTS { HUMANOID,
-					ANIMAL };
+
+    enum GAME_OBJECTS {
+        HUMANOID,
+        ANIMAL
+    };
+
+
+    extern float VERTEX_MM_DARK_BLUE[];
+    
+
+    extern float VERTEX_MM_BLUE[];
+    
+
+    extern float VERTEX_MM_SKIN[];
+    
+
+    extern float VERTEX_MM_FACE[];
+    
+
+    extern float VERTEX_DOG_FRONT_LEGS_RIGHT[];
+    
+
+    extern float VERTEX_DOG_FRONT_LEGS_LEFT[];
+    
+
+    extern float VERTEX_DOG_BACK_LEGS_RIGHT[];
+   
+
+
+    extern float VERTEX_DOG_BACK_LEGS_LEFT[];
+    
+
+    extern float VERTEX_DOG_TAIL[];
+    
+
+    extern float VERTEX_DOG_BODY[];
+    
+
+
+
 

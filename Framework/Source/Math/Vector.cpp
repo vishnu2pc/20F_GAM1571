cb501b37125f995de0fc08907ccd4c209746ac35
Vishnu Jayaraj <vishnu2pc@gmail.com>
2020-09-27 12:37:29 -0400

    Added Vector class

---
+++ Framework/Source/Math/Vector.cpp
@@ -0,0 +1,17 @@
+#include"Vector.h"
+
+vec2::vec2()
+{
+	x = 0;
+	y = 0;
+}
+
+vec2::vec2(float a, float b)
+{
+	x = a;
+	y = b;
+}
+
+vec2::~vec2()
+{
+}
---
+++ Framework/Source/Math/Vector.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class vec2
+{
+public:
+	vec2();
+	vec2(float a, float b);
+	~vec2();
+
+	float x;
+	float y;
+};
+
+
+
